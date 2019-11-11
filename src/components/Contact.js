269a224822745e55af7951e740fdb64573a11aef
dheeraj sharma <dj4385@gmail.com>
2019-11-11 15:23:48 +0530

    contact us page done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,33 +1,30 @@
 import React from 'react'
+import './contact.css'
 
 export const Contact = ()=>{
     return(
 
         <div className="container-fluid bg-success p-5">
-            <h1 className="abtMe">Contact</h1>
+            <h1 className="abtMe">My Contact Details</h1>
             <div className="row">
-                <div className="col-lg-12 mt-4 p-5 eduBg">
+                <div className="col-lg-12 mt-4 p-5">
                     <div className="row">
-                        <div className="col-lg-3">
-                            <div className="card p-3">
-                                Card 1
+                        <div className="col-lg-12">
+                            <div className="card p-5">
+                                <p><b>Email:</b> <br /> &nbsp;&nbsp;&nbsp;&nbsp; <span className="fa fa-envelope-o"></span> dheeraj2406@outlook.com</p>
+                                <p><b>Phone :</b> <br /> &nbsp;&nbsp;&nbsp;&nbsp; <span className="fa fa-phone"></span> +91 8745006149</p>
+                                <p><b>Address:</b> <br /> &nbsp;&nbsp;&nbsp;&nbsp; <span className="fa fa-map-marker"></span>  New Delhi, India</p>
+                                <div className="mt-5">
+                                    <p><b>Socal Links:</b></p>
+                                    <ul className="socialLink">
+                                        <li><a href="#"><span className="fa fa-2x fa-facebook-official"></span></a></li>
+                                        <li><a href="#"><span className="fa fa-2x fa-linkedin-square"></span></a></li>
+                                        <li><a href="#" className="text-dark"><span className="fa fa-2x fa-github"></span></a></li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-lg-3">
-                        <div className="card p-3">
-                                Card 1
-                            </div>
-                        </div>
-                        <div className="col-lg-3">
-                        <div className="card p-3">
-                                Card 1
-                            </div>
-                        </div>
-                        <div className="col-lg-3">
-                        <div className="card p-3">
-                                Card 1
-                            </div>
-                        </div>
+                        
                     </div>
 
                 </div>
