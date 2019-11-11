cfd8723504247bb1c9c0c8069a05846d847b01c0
dheeraj sharma <dj4385@gmail.com>
2019-11-11 12:37:33 +0530

    skill component data fill

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,23 +1,46 @@
 import React from 'react'
 import './Skill.css'
+import angular from '../assets/images/angular.png'
 import express from '../assets/images/express.png'
+import node from '../assets/images/node.png'
+import mongo from '../assets/images/mongo.png'
+import js from '../assets/images/js.png'
+import bs4 from '../assets/images/bootstrap.jpg'
+import html from '../assets/images/html1.jpg'
 import css from '../assets/images/css.jpg'
+
 
 export const Skills = ()=>{
     return (
         <div className="container-fluid bg-dark p-5">
-            <h1 className="abtMe">Skills</h1>
+            <h1 className="text-white abtMe">Skills</h1>
             <div className="row">
                 <div className="col-lg-12 mt-4 pt-4 pl-5 pr-5">
                     <div className="row">
                         <div className="col-lg-6">
                             <div className="card p-3">
-                                <div className="progress-bar bg-danger progress-bar-striped ngprogressbar">Angular 6</div>
+                                <div className="row">
+                                    <div className="col-lg-2">
+                                        <img src={angular} alt="" className="img-fluid skillIcon" />
+                                    </div>
+                                    <div className="col-lg-10">
+                                        <p className="pt-3"><b>Angular 6 and 7</b></p>
+                                        <span className="progress-bar bg-danger progress-bar-striped ngprogressbar">Angular 6</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className="col-lg-6">
                             <div className="card p-3">
-                            <div className="progress-bar bg-primary progress-bar-striped reactprogressbar">Reactjs</div>
+                                <div className="row">
+                                    <div className="col-lg-2">
+                                        <img src={angular} alt="" className="img-fluid skillIcon" />
+                                    </div>
+                                    <div className="col-lg-10">
+                                        <p className="pt-3"><b>Reactjs </b> </p>
+                                        <span className="progress-bar bg-primary progress-bar-striped reactprogressbar">Reactjs</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -29,13 +52,28 @@ export const Skills = ()=>{
                     <div className="row">
                         <div className="col-lg-6">
                             <div className="card p-3">
-                                <div className="progress-bar bg-dark progress-bar-striped nodeprogressbar">Nodejs</div>
+                                <div className="row">
+                                    <div className="col-lg-2">
+                                        <img src={node} alt="" className="img-fluid skillIcon" />
+                                    </div>
+                                    <div className="col-lg-10">
+                                        <p className="pt-3"><b>Nodejs</b></p>
+                                        <span className="progress-bar bg-dark progress-bar-striped nodeprogressbar">Nodejs</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className="col-lg-6">
                             <div className="card p-3">
-                                <img src={express} className="img-fluid skillIcon" alt =""/>
-                                <div className="progress-bar bg-secondary mt-2 progress-bar-striped expressprogressbar">Express</div>
+                                <div className="row">
+                                    <div className="col-lg-2">
+                                        <img src={express} alt="" className="img-fluid skillIcon" />
+                                    </div>
+                                    <div className="col-lg-10">
+                                        <p className="pt-3"><b>Expressjs </b> </p>
+                                        <span className="progress-bar bg-secondary mt-2 progress-bar-striped expressprogressbar">Express</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -46,12 +84,29 @@ export const Skills = ()=>{
                     <div className="row">
                         <div className="col-lg-6">
                             <div className="card p-3">
-                            <   div className="progress-bar bg-success progress-bar-striped mongoprogressbar">MongoDB</div>
+                                <div className="row">
+                                    <div className="col-lg-2">
+                                        <img src={mongo} alt="" className="img-fluid skillIcon" />
+                                    </div>
+                                    <div className="col-lg-10">
+                                        <p className="pt-3"><b>MongoDB </b> </p>
+                                        <span className="progress-bar bg-success progress-bar-striped mongoprogressbar">MongoDB</span>
+                                    </div>
+                                </div>
+                                
                             </div>
                         </div>
                         <div className="col-lg-6">
                             <div className="card p-3">
-                                <div className="progress-bar bg-warning progress-bar-striped jsprogressbar">JavaScript</div>
+                                <div className="row">
+                                    <div className="col-lg-2">
+                                        <img src={js} alt="" className="img-fluid skillIcon" />
+                                    </div>
+                                    <div className="col-lg-10">
+                                        <p className="pt-3"><b>JavaScript </b> </p>
+                                        <span className="progress-bar bg-warning progress-bar-striped jsprogressbar">JavaScript</span>
+                                    </div>
+                                </div>    
                             </div>
                         </div>
                     </div>
@@ -62,13 +117,28 @@ export const Skills = ()=>{
                     <div className="row">
                         <div className="col-lg-6">
                             <div className="card p-3">
-                                <img src={css} className="img-fluid skillIcon" alt =""/>    
-                                <div className="progress-bar bg-danger progress-bar-striped jQueryprogressbar">jQuery</div>
+                                <div className="row">
+                                    <div className="col-lg-2">
+                                        <img src={angular} alt="" className="img-fluid skillIcon" />
+                                    </div>
+                                    <div className="col-lg-10">
+                                        <p className="pt-3"><b>jQuery</b></p>
+                                        <span className="progress-bar bg-danger progress-bar-striped jQueryprogressbar">jQuery</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className="col-lg-6">
                             <div className="card p-3">
-                                <div className="progress-bar bg-success progress-bar-striped bootstrapprogressbar">Bootstrap</div>
+                                <div className="row">
+                                    <div className="col-lg-2">
+                                        <img src={bs4} alt="" className="img-fluid skillIcon" />
+                                    </div>
+                                    <div className="col-lg-10">
+                                        <p className="pt-3"><b>Bootstrap 3 and 4 </b> </p>
+                                        <span className="progress-bar bg-success progress-bar-striped bootstrapprogressbar">Bootstrap</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -80,12 +150,28 @@ export const Skills = ()=>{
                     <div className="row">
                         <div className="col-lg-6">
                             <div className="card p-3">
-                            <div className="progress-bar bg-dark progress-bar-striped htmlprogressbar">HTML</div>
+                                <div className="row">
+                                    <div className="col-lg-2">
+                                        <img src={html} alt="" className="img-fluid skillIcon" />
+                                    </div>
+                                    <div className="col-lg-10">
+                                        <p className="pt-3"><b>HTML</b> </p>
+                                        <span className="progress-bar bg-dark progress-bar-striped htmlprogressbar">HTML</span>
+                                    </div>
+                                </div>  
                             </div>
                         </div>
                         <div className="col-lg-6">
                             <div className="card p-3">
-                            <div className="progress-bar bg-warning progress-bar-striped cssprogressbar">CSS 3</div>
+                                <div className="row">
+                                    <div className="col-lg-2">
+                                        <img src={css} alt="" className="img-fluid skillIcon" />
+                                    </div>
+                                    <div className="col-lg-10">
+                                        <p className="pt-3"><b>CSS 3 </b> </p>
+                                        <span className="progress-bar bg-warning progress-bar-striped cssprogressbar">CSS 3</span>
+                                    </div>
+                                </div>  
                             </div>
                         </div>
                     </div>
