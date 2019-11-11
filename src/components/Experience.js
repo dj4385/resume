03db318eb98a65b4a70bbe1736bf8b7a4dad1c3a
dheeraj sharma <dj4385@gmail.com>
2019-11-11 14:50:02 +0530

    experience component is created

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,30 +5,19 @@ export const Experience = ()=>{
         <div className="container-fluid bg-success p-5">
             <h1 className="abtMe">Experience</h1>
             <div className="row">
-                <div className="col-lg-12 mt-4 p-5 eduBg">
-                    <div className="row">
-                        <div className="col-lg-3">
-                            <div className="card p-3">
-                                Card 1
-                            </div>
-                        </div>
-                        <div className="col-lg-3">
-                        <div className="card p-3">
-                                Card 1
-                            </div>
-                        </div>
-                        <div className="col-lg-3">
-                        <div className="card p-3">
-                                Card 1
-                            </div>
-                        </div>
-                        <div className="col-lg-3">
-                        <div className="card p-3">
-                                Card 1
-                            </div>
-                        </div>
+                <div className="col-lg-6 mt-4 pt-5 pl-5 pb-5">
+                    <div className="card p-5">
+                        <p><span className="fa fa-calendar"></span>  June, 2019 - Present</p>
+                        <p className="h3">Excelsior Softwares Pvt. Ltd.</p>
+                        <p className="small">Bref the job</p>
                     </div>
-
+                </div>
+                <div className="col-lg-6 mt-4 pt-5 pb-5 pr-5">
+                    <div className="card p-5">
+                        <p><span className="fa fa-calendar"></span>  Nov, 2018 - Apr, 2019</p>
+                        <p className="h3">Kranti tech Services Pvt. Ltd.</p>
+                        <p className="small">Bref the job</p>
+                    </div>
                 </div>
             </div>
         </div>
